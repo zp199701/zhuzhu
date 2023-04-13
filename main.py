c7e99929578eb41aff833698b8d1d7458327f083
zp199701 <112870315+zp199701@users.noreply.github.com>
2023-04-13 16:05:09 +0800

    Update main.py

--- main.py
+++ main.py
@@ -109,8 +109,8 @@ def get_tuwei():
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
     }
     r = get(url, headers=headers)
-    a="kk"
-    return r.json()["returnObj"],a
+    a=""
+    return r.text,a
  
 def send_message(to_user, access_token, region_name, weather, temp, wind_dir, note_ch, note_en):
     url = "https://api.weixin.qq.com/cgi-bin/message/template/send?access_token={}".format(access_token)
