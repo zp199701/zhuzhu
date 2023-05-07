e9d4e0d359e14be3016733481af1f0b1013a32de
zp199701 <112870315+zp199701@users.noreply.github.com>
2023-05-07 18:50:07 +0800

    Update main.py

--- main.py
+++ main.py
@@ -259,6 +259,5 @@ if __name__ == "__main__":
         note_ch, note_en = get_tuwei()
     # 公众号推送消息
     for user in users:
-        print(user, accessToken, region, weather, temp, wind_dir, note_ch, note_en)
-        send_message(user1, accessToken, region, weather, temp, wind_dir, note_ch, note_en)
+        send_message(user, accessToken, region, weather, temp, wind_dir, note_ch, note_en)
     os.system("pause")
